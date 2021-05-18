499515f84bcaef68833e61feed77a00a49c1a002
vu.nguyen <vu.nguyen@fidesio.com>
2021-05-18 14:37:10 +0700

    xóa fn

--- public_html/wp-content/themes/mypconect/functions.php
+++ public_html/wp-content/themes/mypconect/functions.php
@@ -3,7 +3,6 @@ define('APP_PATH',dirname(__FILE__));
 /**
  * CORE Includes
 **/
-include APP_PATH.'/inc/Define.php';
 include APP_PATH.'/inc/Setup.php';
 include APP_PATH.'/inc/admin/functions.php';
 include APP_PATH.'/inc/plugins/acf/functions.php';
