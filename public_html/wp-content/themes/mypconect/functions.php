9a3476354615e35301e1fee0a7662405ddf1320f
vu.nguyen <vu.nguyen@fidesio.com>
2021-05-18 14:37:59 +0700

    xoa plg

--- public_html/wp-content/themes/mypconect/functions.php
+++ public_html/wp-content/themes/mypconect/functions.php
@@ -5,6 +5,5 @@ define('APP_PATH',dirname(__FILE__));
 **/
 include APP_PATH.'/inc/Setup.php';
 include APP_PATH.'/inc/admin/functions.php';
-include APP_PATH.'/inc/plugins/acf/functions.php';
 include APP_PATH.'/inc/plugins/wpml/functions.php';
 include APP_PATH.'/inc/frontend/MainMenuWalker.php';
